2938f7a7534bc901807700c6256e7304c53379a4
Andriyaka Sergey <s.andriyaka@1plus1.tv>
2020-01-17 22:11:15 +0200

    almost ready

--- mix_csv.py
+++ mix_csv.py
@@ -4,7 +4,9 @@ import chardet
 from glob import glob
 from typing import List
 import time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+import shutil
+import os
 
 def predict_encoding(file_path, n_lines=20):
     '''Predict a file's encoding using chardet'''
@@ -34,10 +36,9 @@ def csv2table(csv_file, enc, delimiter=";"):
                 row[2] = row[2].zfill(8)
             return table
 
-def HHMMSS(HHMM,delta_minutes,format="%H:%M"):
+def HHMMSS(HHMM,delta_minutes=0,format="%H:%M"):
     hhmm = datetime.strptime(HHMM, format)
-    if delta_minutes:
-        hhmm+=timedelta(minutes=delta_minutes)
+    hhmm+=timedelta(minutes=delta_minutes)
     return hhmm.strftime("%H:%M:%S")
 
 
@@ -46,65 +47,101 @@ def lt(time1,time2,form="%H:%M:%S"):
     # return time.strptime(time1.zfill(8), form) < time.strptime(time1.zfill(8), form)
     return time1 < time2
 
-def week_table_to_playlists(week_table="week_table.csv",channel="1plus1.analog",rolik="mpg",n=2,druration=5,start_date=datetime.today()):
+def week_table_to_playlists(week_table="week_table.csv",
+                            channel="1plus1.analog",
+                            rolik="mpg",
+                            duration=5,n_repeat=2,
+                            obligate_sign="*",
+                            wdays=["понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"],
+                            start_date=None,
+                            date_format="%Y.%m.%d"):
     encoding = predict_encoding(week_table)
     csvs = glob("*.csv")
     with  open(week_table, encoding=encoding, newline='') as week_csv:
         table = list(get_reader(week_csv))[1:]
         for wd in range(7):
-
-            with open(f"{channel}.{str(wd).zfill(2)}.csv","w",newline='') as daycsv:
+            if start_date:
+                yyyymmdd = datetime.strptime(start_date, date_format)
+                week_day = yyyymmdd.weekday()
+                if wd < week_day:
+                    continue
+                yyyymmdd += timedelta(days=wd-week_day)
+                YYMMDD = yyyymmdd.strftime(date_format)
+                file_name = f"{channel}.{YYMMDD}.csv"
+            else:
+                file_name = f"{channel}.{wd+1}.{wdays[wd]}.csv"
+            with open(file_name,"w",newline='') as daycsv:
                 writer = csv.writer(daycsv, delimiter=';')
                 for row in table:
-                    for _ in range(n):
-                        start = HHMMSS(row[wd],-10)
-                        writer.writerow([start,rolik,row[wd]])
+                    for _ in range(n_repeat):
+                        block_start = HHMMSS(row[wd],-1)
+                        play_time = HHMMSS(row[wd])
+                        secs = str(duration*n_repeat)
+                        block_id = ((row[wd].replace(':',''))*2)[:6]
+                        writer.writerow([block_start,rolik,play_time,secs,block_id,obligate_sign])
                     print(row[wd])
 
+def mix_csvs(argv, outcsv=None):
+    if len(argv)>2:
+        return None
+    if argv == None:
+        csv_files = glob("*.csv")[:2]
+    else:
+        csv_files = argv
+    encs = map(predict_encoding, csv_files)
+    tables = [csv2table(csv_file,enc) for csv_file,enc in zip(csv_files,encs)]
+
+    table1, table2 = map(lambda t: list(reversed(t)), tables)
+
+    mixed_table = []
+
+
+    while table1 and table2:
+        row1, row2 = table1[-1], table2[-1]
+        time1, time2 = row1[0], row2[0]
+
+        if lt(time1,time2):
+            if row1:
+                row1 = table1.pop()
+                mixed_table.append(row1)
+                print("tbl1", row1)
+        else:
+            if row2:
+                row2=table2.pop()
+                mixed_table.append(row2)
+                print("tbl2", row2)
+
+    mixed_table+=reversed(table2) if table2 else reversed(table1)
+
+    out = 'out.csv'
+    if outcsv:
+        with open(out, 'w', newline='') as f:
+            writer = csv.writer(f,delimiter=';')
+            writer.writerows(mixed_table)
+    else:
+        with open(out, 'w', newline='') as f:
+            writer = csv.writer(f,delimiter=';')
+            writer.writerows(mixed_table)
+        shutil.copy(argv[0], f"{argv[0]}.bak")
+        os.remove(argv[0])
+        shutil.copy(out,argv[0])
+        os.remove(out)
 
 
 if __name__ == "__main__":
-    # if len(sys.argv)==3:
-    #     csv_files: List[str] = [sys.argv[1], sys.argv[2]]
-    # else:
-    #     csv_files: List[str] = glob("*.csv")[:2]
-    # encs = map(predict_encoding, csv_files)
-    # tables = [csv2table(csv_file,enc) for csv_file,enc in zip(csv_files,encs)]
-    # # print(tables)
-    #
-    # table1, table2 = map(lambda t: list(reversed(t)), tables)
-    #
-    # mixed_table = []
-    #
-    #
-    # while table1 and table2:
-    #     row1, row2 = table1[-1], table2[-1]
-    #     time1, time2 = row1[0], row2[0]
-    #
-    #     if lt(time1,time2):
-    #         if row1:
-    #
-    #             row1 = table1.pop()
-    #             mixed_table.append(row1)
-    #             print("tbl1", row1)
-    #     else:
-    #         if row2:
-    #
-    #             row2=table2.pop()
-    #             mixed_table.append(row2)
-    #             print("tbl2", row2)
-    #
-    #
-    # mixed_table+=reversed(table2) if table2 else reversed(table1)
-    #
-    # with open('out.csv', 'w', newline='') as f:
-    #     writer = csv.writer(f,delimiter=';')
-    #     writer.writerows(mixed_table)
+    week_table_to_playlists(channel="1plus1.analog", rolik="koduvanya_11_ostatochno_28_sichnya")
+    mix_csvs(["1plus1.analog.2020.01.18.csv",
+              "1plus1.analog.6.суббота.csv"])
+    mix_csvs(["1plus1.analog.2020.01.19.csv",
+              "1plus1.analog.7.воскресенье.csv"])
 
-    # print(mixed_table)
-    week_table_to_playlists()
+    week_table_to_playlists(channel="TET.analog", rolik="koduvanya_28_sichnya_tet",start_date="2020.01.17")
 
-
+    week_table_to_playlists(channel="twoplustwo.analog", rolik="koduvanya_sd_2")
+    mix_csvs(["twoplustwo.analog.2020.01.18.csv",
+              "twoplustwo.analog.6.суббота.csv"])
+    mix_csvs(["1plus1.analog.2020.01.19.csv",
+              "twoplustwo.analog.7.воскресенье.csv"])
 
 
 
